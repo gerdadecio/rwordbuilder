b92bc37d74f67f509f4fb645f69278c53c759db9
Arrington.CT <Arrington.CT@32db7296-bf3f-7dea-b8ed-958034bef43f>
2010-12-03 03:37:40 +0000

    changed property injector, added center to paragraph

--- paragraph.rb
+++ paragraph.rb
@@ -1,20 +1,31 @@
 require "element.rb"
+require "property_injector.rb"
 
 # The Paragraph class represents a WordML paragraph element.
 # It is worth noting that Paragraph contains elements, but is not a subtype of Element.
 #
 # see ParagraphTest
 class Paragraph
+  @@p_pr = Element.new("w", "pPr")
+  @@center_injectable = Element.new("w", "jc")
+  @@center_injectable.add_new_attribute("w", "val", "center")
   
-  def initialize(text)
+  CENTER = PropertyInjector.new(@@p_pr, @@center_injectable, true)
+  
+  def initialize(text, *property_injectors)
     @p_element = Element.new("w", "p")
     @p_element.add_new_element("w", "pPr")
     @run_element = @p_element.add_new_element("w", "r")
     @run_element.add_new_element("w", "rPr")
     @run_element.add_new_element("w", "t").add_content(text)
+    
+    property_injectors.each do |injector|
+      injector.inject(@p_element)
+    end
   end
   
   def build_to(buffer)  
     @p_element.build_to(buffer)
   end
+  
 end
--- usage_sample.rb
+++ usage_sample.rb
@@ -1,14 +1,15 @@
 require 'doc.rb'
-require "element.rb"
-require "attribute.rb"
-
+require "paragraph.rb"
 
 buffer = StringIO.new
-doc2 = Doc.new
-doc2.view='print'
-doc2.add_paragraph("Hello World")
+doc = Doc.new
+doc.view='print'
+doc.add_new_paragraph("Hello World")
 
-doc2.build_to(buffer)
+paragraph = Paragraph.new("hi centered", Paragraph::CENTER)
+doc.add_paragraph(paragraph)
+
+doc.build_to(buffer)
 puts "content=\n#{buffer.string}"
 File.open("documents/silly.doc", "w") do |file|
   file.puts buffer.string
